b47931a6c980be6a651a37b594db06434c63d69e
nab880 <nbutche@sandia.gov>
2026-05-20 03:41:01 -0700

    quetz: bundle core stats into QuetzCoreStats struct

---
+++ src/sst/elements/quetz/quetz_stats.cc
@@ -0,0 +1,38 @@
+// Copyright 2009-2026 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2026, NTESS
+// All rights reserved.
+
+#include "quetz_stats.h"
+#include "quetzcore.h"
+
+namespace SST {
+namespace Quetz {
+
+void QuetzCoreStats::registerAll(QuetzCore* comp, const char* sub_id) {
+    read_reqs             = comp->registerStatistic<uint64_t>("read_requests",       sub_id);
+    write_reqs            = comp->registerStatistic<uint64_t>("write_requests",      sub_id);
+    read_lat              = comp->registerStatistic<uint64_t>("read_latency",        sub_id);
+    write_lat             = comp->registerStatistic<uint64_t>("write_latency",       sub_id);
+    read_req_sizes        = comp->registerStatistic<uint64_t>("read_request_sizes",  sub_id);
+    write_req_sizes       = comp->registerStatistic<uint64_t>("write_request_sizes", sub_id);
+    split_reads           = comp->registerStatistic<uint64_t>("split_read_requests", sub_id);
+    split_writes          = comp->registerStatistic<uint64_t>("split_write_requests",sub_id);
+    noop_count            = comp->registerStatistic<uint64_t>("no_ops",              sub_id);
+    insn_count            = comp->registerStatistic<uint64_t>("instruction_count",   sub_id);
+    cycles                = comp->registerStatistic<uint64_t>("cycles",              sub_id);
+    active_cycles         = comp->registerStatistic<uint64_t>("active_cycles",       sub_id);
+    filtered_reads        = comp->registerStatistic<uint64_t>("filtered_reads",      sub_id);
+    filtered_writes       = comp->registerStatistic<uint64_t>("filtered_writes",     sub_id);
+    stall_cycles          = comp->registerStatistic<uint64_t>("stall_cycles",         sub_id);
+    compute_stall_cycles  = comp->registerStatistic<uint64_t>("compute_stall_cycles", sub_id);
+    int_compute           = comp->registerStatistic<uint64_t>("int_compute",         sub_id);
+    fp_compute            = comp->registerStatistic<uint64_t>("fp_compute",          sub_id);
+    vec_compute           = comp->registerStatistic<uint64_t>("vec_compute",         sub_id);
+    branch                = comp->registerStatistic<uint64_t>("branch",              sub_id);
+}
+
+} // namespace Quetz
+} // namespace SST
---
+++ src/sst/elements/quetz/quetz_stats.h
@@ -0,0 +1,50 @@
+// Copyright 2009-2026 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2026, NTESS
+// All rights reserved.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#ifndef _H_SST_QUETZ_STATS
+#define _H_SST_QUETZ_STATS
+
+#include <sst/core/statapi/statbase.h>
+
+namespace SST {
+namespace Quetz {
+
+class QuetzCore;
+
+struct QuetzCoreStats {
+    Statistics::Statistic<uint64_t>* read_reqs        = nullptr;
+    Statistics::Statistic<uint64_t>* write_reqs       = nullptr;
+    Statistics::Statistic<uint64_t>* read_lat         = nullptr;
+    Statistics::Statistic<uint64_t>* write_lat        = nullptr;
+    Statistics::Statistic<uint64_t>* read_req_sizes   = nullptr;
+    Statistics::Statistic<uint64_t>* write_req_sizes  = nullptr;
+    Statistics::Statistic<uint64_t>* split_reads      = nullptr;
+    Statistics::Statistic<uint64_t>* split_writes     = nullptr;
+    Statistics::Statistic<uint64_t>* noop_count       = nullptr;
+    Statistics::Statistic<uint64_t>* insn_count       = nullptr;
+    Statistics::Statistic<uint64_t>* cycles           = nullptr;
+    Statistics::Statistic<uint64_t>* active_cycles    = nullptr;
+    Statistics::Statistic<uint64_t>* filtered_reads   = nullptr;
+    Statistics::Statistic<uint64_t>* filtered_writes  = nullptr;
+    Statistics::Statistic<uint64_t>* stall_cycles         = nullptr;
+    Statistics::Statistic<uint64_t>* compute_stall_cycles = nullptr;
+    Statistics::Statistic<uint64_t>* int_compute      = nullptr;
+    Statistics::Statistic<uint64_t>* fp_compute       = nullptr;
+    Statistics::Statistic<uint64_t>* vec_compute      = nullptr;
+    Statistics::Statistic<uint64_t>* branch           = nullptr;
+
+    void registerAll(QuetzCore* comp, const char* sub_id);
+};
+
+} // namespace Quetz
+} // namespace SST
+
+#endif // _H_SST_QUETZ_STATS
