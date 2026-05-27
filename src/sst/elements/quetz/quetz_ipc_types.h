563a1fe184ad2eba78dbc387824a05481158730b
nab880 <nbutche@sandia.gov>
2026-05-26 22:44:23 -0700

    quetz-balar-gpu-integration-r2 widen QuetzCommand data to cache line
    Expand QuetzCommand::data to 64 bytes so Balar CUDA call packets fit the IPC tunnel.

--- src/sst/elements/quetz/quetz_ipc_types.h
+++ src/sst/elements/quetz/quetz_ipc_types.h
@@ -41,6 +41,8 @@ enum QuetzInsnClass : uint32_t {
     QUETZ_INSN_CLASS_COUNT  = 8
 };
 
+static constexpr unsigned QUETZ_CMD_DATA_BYTES = 64;
+
 struct QuetzCommand {
     QuetzShmemCmd cmd;
     uint32_t      size;
@@ -48,7 +50,7 @@ struct QuetzCommand {
     uint64_t      addr;
     uint32_t      insn_class;
     uint32_t      _pad;
-    uint8_t       data[16];
+    uint8_t       data[QUETZ_CMD_DATA_BYTES];
 };
 
 
