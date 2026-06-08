3b3427ca695c6bf89de3c9b9ff4522f2db49407e
nab880 <nbutche@sandia.gov>
2026-06-07 23:22:21 -0700

    balar: harden Quetz trace and packet wire ABI

--- src/sst/elements/balar/tests/testsuite_default_balar.py
+++ src/sst/elements/balar/tests/testsuite_default_balar.py
@@ -77,13 +77,15 @@ class testcase_balar_simple(BalarTestCase):
     @BalarTestCase.balar_basic_unittest
     def test_quetz_balar_malloc_free(self):
         self.quetz_contract_testcpu_template(
-            "quetz_malloc_free", "testQuetz-balar-malloc-free.py", "malloc_free.trace", testtimeout=60 * 15)
+            "quetz_malloc_free", "testQuetz-balar-malloc-free.py", "malloc_free.trace",
+            testtimeout=60 * 15, min_cuda_calls_completed=3)
 
     @BalarTestCase.balar_basic_unittest
     def test_quetz_balar_wide_packet(self):
         self.quetz_contract_testcpu_template(
             "quetz_wide_packet", "testQuetz-balar-wide-packet.py", "wide_memcpy_d2h.trace",
-            testtimeout=60 * 20, min_flush_count=64)
+            testtimeout=60 * 20, min_flush_count=64, min_cuda_calls_completed=5,
+            min_d2h_bytes=4096)
 
     @unittest.skipIf(
         os.getenv("LLVM_INSTALL_PATH") is None or os.getenv("RISCV_TOOLCHAIN_INSTALL_PATH") is None,
